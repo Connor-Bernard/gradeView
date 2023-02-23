8b97480c6180df4a22495d547fd0ad8734e7b132
Connor Bernard <connorbernard@berkeley.edu>
2023-02-23 03:46:24 -0800

    set up proper rate limiting

--- website/production/index.js
+++ website/production/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const createProxyMiddleware = require('http-proxy-middleware').createProxyMiddleware;
+const { proxy, limit } = require('./middleware');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const app = express();
@@ -11,12 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/api', createProxyMiddleware({target: process.env.REACT_APP_PROXY_SERVER, changeOrigin: true}));
+// Set up API proxy middleware
+app.use('/api', proxy);
 
+// Set up rate limiting middleware
+app.use(limit(10));
+
+// Serve static files from the React app
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ website/production/middleware.js
@@ -0,0 +1,37 @@
+const RateLimit = require('express-rate-limit');
+const createProxyMiddleware = require('http-proxy-middleware').createProxyMiddleware;
+const dotenv = require('dotenv');
+dotenv.config();
+
+// API proxy middleware
+exports.proxy = createProxyMiddleware({ target: process.env.REACT_APP_PROXY_SERVER, changeOrigin: true });
+
+/**
+ * Use to exclude a route from being verified with middleware.
+ * @param {String} path 
+ * @param {Function} middleware 
+ * @returns Function
+ */
+exports.unless = (path, middleware) => {
+    return function (req, res, next) {
+        console.log(path, req.path);
+        if (path === req.path) {
+            return next();
+        } else {
+            return middleware(req, res, next);
+        }
+    }
+}
+
+/**
+ * Use to limit the number of requests made per minute.
+ * @param {int} requests 
+ * @returns {RateLimit} rate limiter middleware
+ */
+exports.limit = (requests) => {
+    return RateLimit({
+        windowMs: 1 * 60 * 1000, // 1 minute
+        max: requests, // limit each IP input requests per minute
+        message: 'Too many requests, please try again after 1 minute'
+    });
+}
