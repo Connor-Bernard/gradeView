34a480f9b649bc09d63cd4a2fb6662f81a30b113
Connor Bernard <connorbernard@berkeley.edu>
2023-02-27 01:39:12 -0800

    Moved locally defined components to individual component files

---
+++ website/src/components/GradeAccordion.js
@@ -0,0 +1,21 @@
+import { Accordion, AccordionSummary, Typography, AccordionDetails } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import GradeTable from "./GradeTable";
+
+export default function GradeAccordion({ category, assignments }) {
+    return (
+        <>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header">
+                    <Typography>{category}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <GradeTable assignments={assignments} />
+                </AccordionDetails>
+            </Accordion>
+        </>
+    );
+}
---
+++ website/src/components/GradeTable.js
@@ -0,0 +1,28 @@
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from "@mui/material";
+
+export default function GradeTable({ assignments }) {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Assignment</TableCell>
+                        <TableCell align="right">Grade</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        assignments.map((row) => (
+                            <TableRow
+                                key={row}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">{row.assignment}</TableCell>
+                                <TableCell align="right">{row.grade}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
