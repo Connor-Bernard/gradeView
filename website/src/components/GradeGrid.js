10b7bfa7659ff78309c0a9fe2da859cc9fd94120
Madelen <madelenf143@berkeley.edu>
2023-03-21 16:15:28 -0700

    forgot to add new grid component

---
+++ website/src/components/GradeGrid.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import GradeTable from './GradeTable';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function GradeGrid({ category, assignments }) {
+
+    const [cumGrade, setCumGrade] = useState(0);
+    const [cumMaxGrade, setCumMaxGrade] = useState(0);
+
+    useEffect(() => {
+        let cg = 0;
+        let cmg = 0;
+        assignments.forEach((assignment) => {
+            cg += +(assignment.grade?.studentGrade || 0);
+            cmg += +(assignment.grade?.maxGrade || 0);
+        });
+        setCumGrade(Math.round(cg * 100) / 100);
+        setCumMaxGrade(Math.round(cmg * 100) / 100);
+    }, [assignments]);
+
+    /**
+     * Returns the formatting for the font-weight.
+     * @param {Float} student 
+     * @param {Float} max 
+     * @returns {String} 'bold' or 'normal'
+     */
+    function isBold(student, max){
+        if(student === max){
+            return 'bold';
+        }
+        return 'normal';
+    }
+
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 3, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          
+          <GradeTable assignments={assignments} />
+        
+      </Grid>
+    </Box>
+  );
+}
