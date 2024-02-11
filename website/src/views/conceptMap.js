fa667eeec3de6fa334adc9859d5bef8a1bec01ad
Dev Ahluwalia <dahluwalia@berkeley.edu>
2024-02-10 22:42:23 -0800

    check 1 concept map loading

--- website/src/views/conceptMap.js
+++ website/src/views/conceptMap.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
 export default function ConceptMap() {
+    const styleFrame = {
+        width: '100%', 
+        border: 'none', 
+        display: 'block', 
+        marginLeft: 'auto', 
+        marginRight: 'auto'
+    };
+
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1>Concept Map</h1>
+            <iframe style={styleFrame} id="ConceptMap" title="Concept Map"
+                src="http://localhost:8080" 
+                allowFullScreen>
+            </iframe>
         </div>
     );
 }
