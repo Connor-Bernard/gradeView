8065da9540e859ddba90cfd53ff068350cfaa556
william-lyh <williamlyh@berkeley.edu>
2024-02-11 18:01:43 -0800

    remove parser UI

--- progressReport/app.py
+++ progressReport/app.py
@@ -66,26 +66,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    course_name = request.args.get("course_name", "CS10")
-    start_date = request.args.get("start_date", "2022-01-01")
-    student_mastery = request.args.get("student_mastery", "000000")
-    parser.generate_map(name=course_name)
-    with open("data/{}.json".format(course_name)) as data_file:
-        course_data = json.load(data_file)
-    course_term = course_data["term"]
-    course_levels = course_data["student levels"]
-    course_node_count = course_data["count"]
-    course_nodes = course_data["nodes"]
-    course_level_colors = [c for n, c in course_levels.items()]
-    return render_template("web_ui.html",
-                           start_date=start_date,
-                           student_mastery=student_mastery,
-                           course_name=course_name,
-                           course_term=course_term,
-                           course_levels=course_levels,
-                           course_level_colors=course_level_colors,
-                           course_node_count=course_node_count,
-                           course_data=course_nodes)
+    return ""
 
 
 @app.route('/parse', methods=["POST"])
