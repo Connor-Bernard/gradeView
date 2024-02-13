85d6f8a25a27d6315eacf2ac51fa03d85a3bee4d
Connor <78193645+Connor-Bernard@users.noreply.github.com>
2024-02-13 00:32:19 -0800

    iframe integration v1 (#96)
    * iframe integration v1
    
    * moved css file correct branch
    
    * Update app.css
    
    * css file import into conceptMap.js
    
    * feat: configure iframe with rev proxy
    
    * iframe seamless integration
    
    ---------
    
    Co-authored-by: Dev Ahluwalia <dahluwalia@berkeley.edu>

--- website/src/views/conceptMap.js
+++ website/src/views/conceptMap.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './css/conceptMap.css';
 
 export default function ConceptMap() {
+
     return (
-        <div>
+        <div style={{ textAlign: 'center' }} overflow="hidden">
             <h1>Concept Map</h1>
+            <iframe className="concept_map_iframe" id="ConceptMap" title="Concept Map" scrolling="no"
+                src="http://localhost:8080?show_legend=false" 
+                allowFullScreen>
+            </iframe>
         </div>
     );
 }
+
