889d212ebb56283c5b4745e051ab767dc7adfa4c
Dev Ahluwalia <dahluwalia@berkeley.edu>
2024-02-13 00:30:03 -0800

    iframe seamless integration

--- website/src/views/conceptMap.js
+++ website/src/views/conceptMap.js
@@ -4,9 +4,9 @@ import './css/conceptMap.css';
 export default function ConceptMap() {
 
     return (
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center' }} overflow="hidden">
             <h1>Concept Map</h1>
-            <iframe className="concept_map_iframe" id="ConceptMap" title="Concept Map"
+            <iframe className="concept_map_iframe" id="ConceptMap" title="Concept Map" scrolling="no"
                 src="http://localhost:8080?show_legend=false" 
                 allowFullScreen>
             </iframe>
