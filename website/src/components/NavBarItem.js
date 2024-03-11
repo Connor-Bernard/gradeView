0353b076c9549c8fa86ce5ce8e539b061dbda209
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-03-10 23:11:10 -0700

    Add activeTab highlighting and onHover coloring for tab selection

--- website/src/components/NavBarItem.js
+++ website/src/components/NavBarItem.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Button, Link } from '@mui/material';
+import { NavLink, useMatch } from 'react-router-dom';
 
 export default function NavBarItem({ href, children }){
+
+    const match = useMatch(href);
     return (
-        <Link href={href} color='inherit' underline='none'>
-            <Button color='inherit'>{children}</Button>
+        <Link component={NavLink} to={href} color='inherit' underline='none'>
+          <Button
+            color='inherit'
+            sx={{
+              backgroundColor: match ? '#1F7CD5' : 'inherit',
+              '&:hover': {
+                backgroundColor: 'lightblue', 
+              }
+            }}
+          >
+            {children}
+          </Button>
         </Link>
-    );
+      );
 }
