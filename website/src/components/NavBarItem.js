7b5efb84996ab32f3c6c7c3b029d7d5a74cf9211
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-03-13 16:19:28 -0700

    Add underline and opacity to active Tab

--- website/src/components/NavBarItem.js
+++ website/src/components/NavBarItem.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { Button, Link } from '@mui/material';
+import { Button, Link, Box } from '@mui/material';
 import { NavLink, useMatch } from 'react-router-dom';
 
 export default function NavBarItem({ href, children }){
 
     const match = useMatch(href);
     return (
-        <Link component={NavLink} to={href} color='inherit' underline='none'>
-          <Button
-            color='inherit'
-            sx={{
-              backgroundColor: match ? '#1F7CD5' : 'inherit',
-              '&:hover': {
-                backgroundColor: 'lightblue', 
-              }
-            }}
-          >
+        <Link component={NavLink} to={href} color='inherit' >
+          <Button sx={{ color: 'inherit', opacity: match ? 1 : 0.7 }}>
             {children}
-          </Button>
-        </Link>
-      );
+            {match && (
+              <Box
+                className="underline"
+                sx={{
+                  position: 'absolute',
+                  bottom: 0, // Position at the bottom of the button
+                  left: '50%',
+                  transform: 'translateX(-50%)', // Center the Box
+                  width: '100%',
+                  height: '2px', // Thickness of the underline
+                  backgroundColor: 'currentColor', // Use the text color for the underline
+                }}
+              />
+          )}
+        </Button>
+      </Link>
+    );
 }
