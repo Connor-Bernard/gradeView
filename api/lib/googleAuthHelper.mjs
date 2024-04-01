c2b356368c1cfc9ea0a16d0df80a64fc068efc18
Connor Bernard <connorbernard@berkeley.edu>
2024-03-31 23:33:25 -0700

    docs: document google auth helper file

--- api/lib/googleAuthHelper.mjs
+++ api/lib/googleAuthHelper.mjs
@@ -2,15 +2,20 @@ import config from "config";
 import AuthenticationError from "./Errors/AuthorizationError.js";
 import { OAuth2Client } from "google-auth-library";
 
+/**
+ * Gets an email from a google auth token.
+ * @param {string} token user token to retrieve email from.
+ * @returns {string} user's email.
+ */
 export async function getEmailFromAuth(token) {
     try {
-        let oauthClient = new OAuth2Client(config.get('googleconfig.oauth.clientid'))
+        let oauthClient = new OAuth2Client(config.get('googleconfig.oauth.clientid'));
         const ticket = await oauthClient.verifyIdToken({
             idToken: token,
-            audience: config.get('googleconfig.oauth.clientid')
+            audience: config.get('googleconfig.oauth.clientid'),
         });
         const payload = ticket.getPayload();
-        if(payload['hd'] !== 'berkeley.edu'){
+        if (payload['hd'] !== 'berkeley.edu') {
             throw new AuthenticationError('domain mismatch');
         }
         return payload['email'];
@@ -19,11 +24,23 @@ export async function getEmailFromAuth(token) {
     }
 }
 
+/**
+ * Ensures that an email is a properly formatted berkeley email.
+ * @param {string} email email to verify.
+ * @returns {boolean} success of verification.
+ */
 export function verifyBerkeleyEmail(email) {
     return email.split("@").length === 2
             && email.split("@")[1] === "berkeley.edu";
 }
 
+// TODO: check if the user is included in the list of users (in the db);
+/**
+ * Checks to see if an email is a student email or an admin.
+ * @param {string} email email to check access to.
+ * @returns {boolean} whether the email is an admin or student.
+ */
 export function ensureStudentOrAdmin(email) {
-    return config.get('admins').includes(email)
+    const isAdmin = config.get('admins').includes(email);
+    return isAdmin;
 }
