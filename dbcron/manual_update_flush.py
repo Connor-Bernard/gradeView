aaa8e89cef9b21dc4a7890813829be31cb2ef429
Dev Ahluwalia <devahluwalia34@gmail.com>
2024-04-22 18:26:02 -0700

    triggers manual redis update on container launch

--- dbcron/manual_update_flush.py
+++ dbcron/manual_update_flush.py
@@ -1,11 +1,11 @@
-import update_db
-import flush_db
-import update_bins
+from update_db import update_redis
+from flush_db import flush_redis_db
+from update_bins import update_bins
 import redis
 import os
 from dotenv import load_dotenv
 load_dotenv()
 if __name__ == "__main__":
-    flush_db()
-    update_db()
+    flush_redis_db()
+    update_redis()
     update_bins()
