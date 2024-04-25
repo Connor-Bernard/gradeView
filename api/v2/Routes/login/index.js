92e8f5c4c93d7e83d8085838005b13c804889e50
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-04-24 23:16:52 -0700

    Add rate limit

--- api/v2/Routes/login/index.js
+++ api/v2/Routes/login/index.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { validateAdminOrStudentMiddleware } from '../../../lib/authlib.mjs';
 import 'express-async-errors';
+import RateLimit from 'express-rate-limit';
 
 const router = Router({ mergeParams: true });
+
+router.use(RateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // 100 requests
+}));
 
 router.get('/', validateAdminOrStudentMiddleware, async (_, res) => {
     res.send({ status: true });
