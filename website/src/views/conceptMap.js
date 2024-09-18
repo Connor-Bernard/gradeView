fdef8cf25dfc7fe2c6a6851ce4bbe1b02f8e87b7
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-09-17 22:18:22 -0700

    Add documentation for ConceptMap.js and add scrolling

--- website/src/views/conceptMap.js
+++ website/src/views/conceptMap.js
@@ -7,17 +7,38 @@ import jwtDecode from 'jwt-decode';
 import { StudentSelectionContext } from "../components/StudentSelectionWrapper";
 import apiv2 from "../utils/apiv2";
 
+/**
+ * The ConceptMap component renders a concept map based on student progress data from the progressQueryString API.
+ * 1. This fetches data either for either:
+ *    a. A currently logged-in user (student view)
+ *    b. A selected student (instructor view)
+ * and displays the concept map within an iframe.
+ * 2. The concept map iframe src takes in a string of numbers to display a concept map,
+ *    a. This makes an API call to the Python Flask application to create the concept map.
+ *    b. Each number represents a student's mastery level for a particular concept. 
+ * 3. The concept nodes are arranged vertically from top to bottom.
+ * 4. The list of numerical strings associated with each node is sorted horizontally from left to right.
+ *    a. This numerical string is calculated through the Google Sheets data in the JavaScript API call.
+ * @component
+ * @returns {JSX.Element} The ConceptMap component.
+ */
 export default function ConceptMap() {
     const [loading, setLoading] = useState(false);
     const [studentMastery, setStudentMastery] = useState('000000');
 
     const { selectedStudent, setSelectedStudent } = useContext(StudentSelectionContext);
 
+    /**
+     * Fetch the logged-in student's mastery data on component mount (student view).
+     * This effect fetches data based on the JWT token stored 
+     * in localStorage and updates the component's state.
+     */
     useEffect(() => {
         let mounted = true;
         setLoading(true);
         if (mounted && localStorage.getItem('token')) {
             let email = jwtDecode(localStorage.getItem('token')).email;
+            // Fetch the student progressQueryString
             apiv2.get(`/students/${email}/progressquerystring`).then((res) => {
                 setStudentMastery(res.data);
                 setLoading(false);
@@ -28,6 +49,11 @@ export default function ConceptMap() {
         return () => mounted = false;
     }, []);
 
+    /**
+     * Fetch the selected student's mastery data whenever the selected student
+     * changes for the instructor view.
+     * This effect depends on the `selectedStudent` from the context.
+     */
     useEffect(() => {
         let mounted = true;
         if (mounted) {
@@ -44,6 +70,11 @@ export default function ConceptMap() {
         return <Loader />;
     }
 
+    /**
+     * Render the concept map iframe with the fetched mastery data.
+     * This iframe src takes in a string of numbers 
+     * (progressQueryString) to display a concept map.
+     */
     return (
         <>
             <PageHeader>Concept Map</PageHeader>
@@ -52,7 +83,6 @@ export default function ConceptMap() {
                     className="concept_map_iframe"
                     id="ConceptMap"
                     title="Concept Map"
-                    scrolling="no"
                     src={`${window.location.origin}/progress?show_legend=false&student_mastery=${studentMastery}`}
                     allowFullScreen
                 />
