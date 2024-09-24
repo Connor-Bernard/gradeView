b13ae5f4043fd176d1b49775296b69df4b34b1f2
Connor <78193645+Connor-Bernard@users.noreply.github.com>
2024-09-23 20:01:15 -0700

    fix: repair iframe scrolling and cm page (#160)

--- website/src/views/conceptMap.js
+++ website/src/views/conceptMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import Loader from '../components/Loader';
 import PageHeader from '../components/PageHeader';
 import './css/conceptMap.css';
@@ -11,7 +11,17 @@ export default function ConceptMap() {
     const [loading, setLoading] = useState(false);
     const [studentMastery, setStudentMastery] = useState('000000');
 
-    const { selectedStudent, setSelectedStudent } = useContext(StudentSelectionContext);
+    const iframeRef = useRef(null);
+
+    const { selectedStudent } = useContext(StudentSelectionContext);
+
+    const handleLoad = useCallback(() =>{
+        if(iframeRef.current) {
+            const iframeDocument = iframeRef.current.contentDocument || iframeRef.current.contentWindow.document;
+            const height = iframeDocument.body.scrollHeight;
+            iframeRef.current.style.height = `${height}px`;
+        }
+    }, []);
 
     useEffect(() => {
         let mounted = true;
@@ -49,11 +59,13 @@ export default function ConceptMap() {
             <PageHeader>Concept Map</PageHeader>
             <div style={{ textAlign: 'center', height:"100%" }} overflow="hidden">
                 <iframe
+                    ref={iframeRef}
                     className="concept_map_iframe"
                     id="ConceptMap"
                     title="Concept Map"
-                    scrolling="no"
                     src={`${window.location.origin}/progress?show_legend=false&student_mastery=${studentMastery}`}
+                    onLoad={handleLoad}
+                    scrolling='no'
                     allowFullScreen
                 />
             </div>
