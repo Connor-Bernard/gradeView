43456fe4e412df09aa4c45b74f7729ed2683014f
Connor Bernard <connorbernard@berkeley.edu>
2024-09-27 18:55:49 -0700

    refactor: rewrite the errors for simplicity and add error stack

--- api/errors/redis/KeyNotFound.js
+++ api/errors/redis/KeyNotFound.js
@@ -4,17 +4,18 @@ import Base from './Base.js';
  * Thrown when a key is not found in Redis.
  */
 export default class KeyNotFoundError extends Base {
-    constructor(keyName, databaseIndex) {
-        if (typeof keyName !== 'string') {
-            throw new Error('keyName must be a string');
-        }
-
-        let smartMessage = "";
-        if (keyName) {
-            smartMessage += `Key "${keyName}" was not found in databaseIndex ${databaseIndex}`;
-        }
-        super(smartMessage);
+    /**
+     * Creates a new KeyNotFoundError.
+     * @constructor
+     * @param {string} message - The base error message.
+     * @param {string} keyName - The key that was queried.
+     * @param {int} databaseIndex - The index of the db that was queried.
+     * @param {Error|null} [err=null] - the existing error that was thrown if any.
+     */
+    constructor(message, keyName, databaseIndex, err=null) {
+        super(`${message}; key '${keyName}' not found in database with index ${databaseIndex}`, err);
         this.name = 'KeyNotFoundError';
         this.keyName = keyName;
+        this.databaseIndex = databaseIndex;
     }
 }
--- api/errors/redis/StudentNotEnrolled.js
+++ api/errors/redis/StudentNotEnrolled.js
@@ -4,16 +4,15 @@ import Base from './Base.js';
  * Thrown when a student is not enrolled in a course or program.
  */
 export default class StudentNotEnrolledError extends Base {
-    constructor(message, studentEmail) {
-        let smartMessage = message;
-        
-        // Append additional details if available
-        if (studentEmail) {
-            smartMessage += `; student "${studentEmail}" is not enrolled`;
-        }
-
-        // Call the Base class constructor with the smart message
-        super(smartMessage);
+    /**
+     * Creates a new StudentNotEnrolledError.
+     * @constructor
+     * @param {string} message - The base error message.
+     * @param {string} studentEmail - The queried student email.
+     * @param {Error|null} err [err=null] - the existing error that was thrown if any.
+     */
+    constructor(message, studentEmail, err=null) {
+        super(`${message}; student '${studentEmail}' is not enrolled`, err);
         this.name = 'StudentNotEnrolledError';
         this.studentEmail = studentEmail;
     }
